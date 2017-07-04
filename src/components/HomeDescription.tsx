0653b1821e4146cfbbf636b012d7b4243a276f36
Ernest Ho <ernestho.ho@gmail.com>
2017-07-03 21:44:50 -0400

    Add login/register buttons

--- src/components/HomeDescription.tsx
+++ src/components/HomeDescription.tsx
@@ -4,15 +4,33 @@ import '../../node_modules/react-typist/dist/Typist.css';
 import * as React from 'react';
 import Typist from 'react-typist';
 
+interface Props {
+  onComplete?: () => void;
+}
+
 const cursor = {
   show: false
 };
 
-export default class HomeDescription extends React.Component {
+export default class HomeDescription extends React.Component<Props> {
+  static defaultProps: Props = {
+    onComplete: () => {}
+  };
+
   render() {
+    const { onComplete } = this.props;
     return (
       <div className="HomeDescription">
-        <Typist className="HomeDescription-text" avgTypingDelay={50} startDelay={500} cursor={cursor}>Code against the world!</Typist>
+        <Typist
+          className="HomeDescription-text"
+          avgTypingDelay={50}
+          stdTypingDelay={0}
+          startDelay={500}
+          cursor={cursor}
+          onTypingDone={() => onComplete && onComplete()}
+        >
+          Code against the world!
+        </Typist>
       </div>
     );
   }
--- src/components/pages/HomePage.tsx
+++ src/components/pages/HomePage.tsx
@@ -2,14 +2,45 @@ import './HomePage.scss';
 
 import * as React from 'react';
 
-import Logo from '../Logo/Logo';
+import Button from '../ui/Button';
 import HomeDescription from '../HomeDescription';
+import Logo from '../Logo/Logo';
 
-const HomePage = () => (
-  <div className="HomePage">
-    <Logo />
-    <HomeDescription />
-  </div>
-);
+interface State {
+  showButtons: boolean;
+}
 
-export default HomePage;
+export default class HomePage extends React.Component<undefined, State> {
+  state: State = {
+    showButtons: false
+  };
+
+  handleTypingComplete = () => {
+    setTimeout(() => {
+      this.setState({ showButtons: true });
+    }, 200);
+  }
+
+  render() {
+    return (
+      <div className="HomePage">
+        <div className="HomePage-logo">
+          <Logo />
+          <HomeDescription onComplete={this.handleTypingComplete} />
+        </div>
+        <div className="Homepage-buttons">
+          {
+            this.state.showButtons
+            ? (
+                <span>
+                  <Button primary>Login</Button>
+                  <Button outline>Register</Button>
+                </span>
+              )
+            : null
+          }
+        </div>
+      </div>
+    );
+  }
+}
