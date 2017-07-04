ba60393ac416934bd09aec4b970ff165ccac24ec
Ernest Ho <ernestho.ho@gmail.com>
2017-07-03 22:50:01 -0400

    Animate buttons on home page

--- src/components/pages/HomePage.tsx
+++ src/components/pages/HomePage.tsx
@@ -1,44 +1,52 @@
 import './HomePage.scss';
 
 import * as React from 'react';
+import * as classnames from 'classnames';
 
 import Button from '../ui/Button';
 import HomeDescription from '../HomeDescription';
 import Logo from '../Logo/Logo';
 
 interface State {
-  showButtons: boolean;
+  showLogin: boolean;
+  showRegister: boolean;
 }
 
 export default class HomePage extends React.Component<undefined, State> {
   state: State = {
-    showButtons: false
+    showLogin: false,
+    showRegister: false,
   };
 
   handleTypingComplete = () => {
     setTimeout(() => {
-      this.setState({ showButtons: true });
+      this.setState({ showLogin: true });
+
+      setTimeout(() => {
+        this.setState({ showRegister: true });
+      }, 200)
     }, 200);
   }
 
   render() {
+    const buttonsCssClass = classnames('Homepage-buttons', {
+      'Homepage-buttons--show-login': this.state.showLogin,
+      'Homepage-buttons--show-register': this.state.showRegister
+    });
+
     return (
       <div className="HomePage">
         <div className="HomePage-logo">
           <Logo />
           <HomeDescription onComplete={this.handleTypingComplete} />
         </div>
-        <div className="Homepage-buttons">
-          {
-            this.state.showButtons
-            ? (
-                <span>
-                  <Button primary>Login</Button>
-                  <Button outline>Register</Button>
-                </span>
-              )
-            : null
-          }
+        <div className={buttonsCssClass}>
+          <span className="HomePage-buttons-login">
+            <Button primary>Login</Button>
+          </span>
+          <span className="HomePage-buttons-register">
+            <Button outline>Register</Button>
+          </span>
         </div>
       </div>
     );
